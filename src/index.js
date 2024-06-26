7a0f72a2cd4135fbad59c0e9a11d2cb9285c03bb
viadel13 <shababizetre@gmail.com>
2024-06-26 10:44:12 +0100

    first commit

--- src/index.js
+++ src/index.js
@@ -4,6 +4,18 @@ import './index.css';
 import App from './App';
 import '@fontsource/manrope'; 
 
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker
+      .register('/service-worker.js')
+      .then(registration => {
+        console.log('Service Worker registered with scope: ', registration.scope);
+      })
+      .catch(err => {
+        console.log('Service Worker registration failed: ', err);
+      });
+  });
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
